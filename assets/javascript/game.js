1dc4202a25d1799769191220cf30e5bdffa2a2e2
Badger-MchlB <boynton.mchl@gmail.com>
2019-02-24 14:47:50 -0500

    Fixed click functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,65 @@
 $(document).ready(function() {
-    console.log( "ready!" );
-});
-var random = Math.floor(Math.random()*102+19);
-$("#numberToGet").text(random);
+  console.log("ready!");
+  crystalClick();
 
-var userTotal= 0;
-var wins= 0;
+var targetScore = Math.floor(Math.random() * 102 + 19);
+$("#numberToGet").text(targetScore);
+
+var yourScore = 0;
+var wins = 0;
 var losses = 0;
-var images = ["../images/Ruby.png","../images/Emerald.png","../images/Amethyst.png","../images/Sapphire.png"];
+var images = [
+  "../images/Ruby.png",
+  "../images/Emerald.png",
+  "../images/Amethyst.png",
+  "../images/Sapphire.png"
+];
 
 $("#numberWins").text(wins);
 $("#numberLosses").text(losses);
-
-function reset(){
-    random = Math.floor(Math.random()*102+19);
-    $("#numberToGet").text(random);
-
-var crystalValues = {};
-crystalValues[1] = Math.floor(Math.random()*12+1);
-crystalValues[2] = Math.floor(Math.random()*12+1);
-crystalValues[3] = Math.floor(Math.random()*12+1);
-crystalValues[4] = Math.floor(Math.random()*12+1);
-
-function getCrystalHandler(crystalKey){
-    return function() {
-        userTotal = yourScore + crystalValues[crystalKey];
-        console.log("New userTotal" + userTotal);
-        $("#score#").text(userTotal);
-
-        if (userTotal === random) {
-            alert ("Winner, winner chicken dinner!");
-            wins++;
-            $("#numberWins").text(wins);
-            reset();
-        } else if (userTotal > targetInt){
-            alert ("Big loser!");
-            losses++;
-            $("#numberLosses").text(losses);
-            reset();
-        }
-    }
+function reset() {
+  targetScore = Math.floor(Math.random() * 102 + 19);
+  $("#targetScore").text(targetScore);
 }
+  var crystalValues = {
+    ruby: Math.floor(Math.random() * 12 + 1),
+    emerald: Math.floor(Math.random() * 12 + 1),
+    amethyst: Math.floor(Math.random() * 12 + 1),
+    sapphire: Math.floor(Math.random() * 12 + 1)
+  };
 
-    
-$("#Ruby").on("click", getCrystalHandler(1));
-$("#Amethyst").on("click", getCrystalHandler(2));
-$("#Emerald").on("click", getCrystalHandler(3));
-$("#Sapphire").on("click", getCrystalHandler(4));
-}
+  function getCrystalHandler(key) {
+      $("#yourScore").text(yourScore);
+      $("#targetScore").text(targetScore);
+      if (yourScore === targetScore) {
+        alert("Winner, winner chicken dinner!");
+        wins++;
+        $("#numberWins").text(wins);
+        reset();
+      } else if (yourScore > targetScore) {
+        alert("Big loser!");
+        losses++;
+        $("#numberLosses").text(losses);
+        reset();
+      }
+  }
 
+  function crystalClick() {
+    $("#ruby").click(function() {
+      yourScore = yourScore + crystalValues["ruby"];
+      getCrystalHandler();
+    });
+    $("#emerald").click(function() {
+      yourScore = yourScore + crystalValues["emerald"];
+      getCrystalHandler();
+    });
+    $("#amethyst").click(function() {
+      yourScore = yourScore + crystalValues["amethyst"];
+      getCrystalHandler();
+    });
+    $("#sapphire").click(function() {
+      yourScore = yourScore + crystalValues["sapphire"];
+      getCrystalHandler();
+    });
+  }
+});
